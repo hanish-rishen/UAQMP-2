9ff5dcf67e75a7a6b8cf553c7af15ffc8765ad56
Hanish Rishen <110720727+hanish-rishen@users.noreply.github.com>
2025-03-14 15:42:14 +0530

    Deployment Errors Fixed

--- src/components/ScriptLoader.tsx
+++ src/components/ScriptLoader.tsx
@@ -1,16 +1,16 @@
-'use client';
-import Script from 'next/script';
+"use client";
+import Script from "next/script";
 
 export default function ScriptLoader() {
   return (
     <>
       <Script
         src="https://cdn.maptiler.com/maptiler-geocoder/v1.1.0/maptiler-geocoder.js"
-        strategy="beforeInteractive"
+        strategy="afterInteractive" // Changed from beforeInteractive to afterInteractive
       />
       <Script
         src="https://api.maptiler.com/geocoding/v1/geocoding.js?key=MakFZUKozVFQcAOahegc"
-        strategy="beforeInteractive"
+        strategy="afterInteractive" // Changed from beforeInteractive to afterInteractive
       />
     </>
   );
